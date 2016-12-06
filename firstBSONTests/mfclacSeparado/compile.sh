e906b8b22470332f3709fb682c620dc032ba0827
Diego Reiriz Cores <diegoreiriz@gmail.com>
2016-12-06 14:15:19 +0100

    Small change

--- firstBSONTests/mfclacSeparado/compile.sh
+++ firstBSONTests/mfclacSeparado/compile.sh
@@ -4,4 +4,4 @@ rm fin.out
 
 bison -d mfcalc.y
 flex --header-file=lex.yy.h lexicalAnalyzer.l
-gcc mfcalc.tab.c lex.yy.c errors.c SymbolTable.c main.c preload.c -lm  -o fin.out
+gcc mfcalc.tab.c lex.yy.c errors.c SymbolTable.c main.c preload.c -lm  -o CLIMath.out
